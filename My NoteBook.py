383c330ecd8630b129864479c78b258d751901c7
SrilathaStephen <119886401+SrilathaStephen@users.noreply.github.com>
2023-01-08 02:41:57 +0530

    Add files via upload

---
+++ My NoteBook.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# My Jupyter Notebook on IBM Studio
+
+# **Srilatha Stephen**
+# Data Scientist
+
+# *I am interested in data science because it is a world of endless possibilities.*
+
+# **The code in cell 5 will give the total when you add 56 + 34**
+
+# In[1]:
+
+
+print(56+34)
+
+
+# | Header | Header|
+# | ------ | ------|
+# | cell   | cell  |
+# | cell2  | cell2 |
+
+# In[ ]:
+
+
+
+
